0dcce0c88e9c10a446444142ded4f660bfed669b
Javier <javier@simplekyc.com>
2024-10-27 10:48:37 +0000

    select period

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+from bidict import bidict
 
 #TODO allow for different currencies. This may require to read the currency from the csv and convert to a common currency
 #TODO allow for different date granularity. Currently only monthly is supported.
 #TODO add options to each individual plot to show/hide data and date granularity (should be possible to clear all filters and reset to global options)
 #TODO add more plots and data about expenses
 #TODO Grab data from database. Upload the csv to the database. 
-
+periods = bidict({'D': 'Daily', 'W': 'Weekly', 'M': 'Monthly', 'Q': 'Quarterly', 'Y': 'Yearly'})
+period_name = bidict({'D': 'Day', 'W': 'Week', 'M': 'Month', 'Q': 'Quarter', 'Y': 'Year'})
 
 # Function to load and process the uploaded CSV files
 def load_data(uploaded_files):
@@ -34,48 +35,48 @@ def adjust_data(base_data, adjustments, adjustment_column=None):
     return adjusted_data
 
 # Modified filter_and_adjust_data function
-def filter_and_adjust_data(combined_data):
+def filter_and_adjust_data(combined_data, period='M'):
     filtered_data = combined_data[~combined_data['Description'].str.contains("GBP to", na=False)]
-    filtered_monthly_balance = filtered_data.groupby(filtered_data['Date'].dt.to_period('M')).last()['Running Balance']
+    filtered_periodic_balance = filtered_data.groupby(filtered_data['Date'].dt.to_period(period)).last()['Running Balance']
 
     removed_rows = combined_data[combined_data['Description'].str.contains("GBP to", na=False)]
     removed_rows['Adjusted Amount'] = -removed_rows['Amount']
-    monthly_adjustments = removed_rows.groupby(removed_rows['Date'].dt.to_period('M'))['Adjusted Amount'].sum()
+    periodic_adjustments = removed_rows.groupby(removed_rows['Date'].dt.to_period(period))['Adjusted Amount'].sum()
 
-    adjusted_monthly_balance = adjust_data(filtered_monthly_balance, monthly_adjustments)
+    adjusted_periodic_balance = adjust_data(filtered_periodic_balance, periodic_adjustments)
     
-    return filtered_monthly_balance, adjusted_monthly_balance
+    return filtered_periodic_balance, adjusted_periodic_balance
 
 # Modified adjust_income_expenses function
-def adjust_income_expenses(monthly_data, adjustments):
-    return adjust_data(monthly_data, abs(adjustments), adjustment_column='Income')
+def adjust_income_expenses(periodic_data, adjustments):
+    return adjust_data(periodic_data, abs(adjustments), adjustment_column='Income')
 
 # Function to calculate net changes
-def calculate_net_changes(monthly_balance):
-    return monthly_balance.diff()
+def calculate_net_changes(periodic_balance):
+    return periodic_balance.diff()
 
 #Function to calculate cumulative net changes
-def calculate_cumulative_net_changes(monthly_balance):
-    return monthly_balance.cumsum()
+def calculate_cumulative_net_changes(periodic_balance):
+    return periodic_balance.cumsum()
 
 # Function to calculate percentage changes
-def calculate_percentage_changes(monthly_balance):
-    return monthly_balance.pct_change() * 100
+def calculate_percentage_changes(periodic_balance):
+    return periodic_balance.pct_change() * 100
 
-# Function to calculate monthly income and expenses
-def calculate_monthly_income_expenses(combined_data):
+# Function to calculate periodic income and expenses
+def calculate_periodic_income_expenses(combined_data, period='M'):
     # Convert Date to period for monthly grouping
-    combined_data['Month'] = combined_data['Date'].dt.to_period('M')
+    combined_data['Period'] = combined_data['Date'].dt.to_period(period)
     
-    # Calculate monthly income and expenses
-    monthly_data = combined_data.groupby('Month').agg({
+    # Calculate periodic income and expenses
+    periodic_data = combined_data.groupby('Period').agg({
         'Amount': lambda x: (x[x > 0].sum(), x[x < 0].sum())
     })
     
-    monthly_data['Income'] = monthly_data['Amount'].apply(lambda x: x[0])
-    monthly_data['Expenses'] = monthly_data['Amount'].apply(lambda x: abs(x[1]))
+    periodic_data['Income'] = periodic_data['Amount'].apply(lambda x: x[0])
+    periodic_data['Expenses'] = periodic_data['Amount'].apply(lambda x: abs(x[1]))
     
-    return monthly_data[['Income', 'Expenses']]
+    return periodic_data[['Income', 'Expenses']]
 
 def calculate_cumulative_percentage_change(data):
     first_value = data.iloc[0]
@@ -85,30 +86,30 @@ def calculate_cumulative_percentage_change(data):
     return cumulative_percentage_change
 
 # Function to plot histogram of income and expenses
-def plot_histogram(monthly_balance, adjusted_monthly_balance, adjusted=False, expenses=False, income=False):
+def plot_histogram(periodic_balance, adjusted_periodic_balance, period='M', adjusted=False, expenses=False, income=False):
     plt.figure(figsize=(12, 6))
     
-    x = np.arange(len(monthly_balance.index))
+    x = np.arange(len(periodic_balance.index))
     width = 0.25  # width of each bar
     
     if adjusted:
-        plt.bar(x - width, adjusted_monthly_balance['Income'], 
+        plt.bar(x - width, adjusted_periodic_balance['Income'], 
                 width, label='Adjusted Income', color='green', alpha=0.7)
     
     if income:
-        plt.bar(x, monthly_balance['Income'], 
+        plt.bar(x, periodic_balance['Income'], 
                 width, label='Income', color='blue', alpha=0.8)
     
     if expenses:
-        plt.bar(x + width, monthly_balance['Expenses'], 
+        plt.bar(x + width, periodic_balance['Expenses'], 
                 width, label='Expenses', color='red', alpha=0.6)
     
-    plt.title("Monthly Income and Expenses")
-    plt.xlabel('Month')
+    plt.title(f"{periods[period]} Income and Expenses")
+    plt.xlabel(periods[period])
     plt.ylabel('Amount (GBP)')
     plt.legend()
     plt.grid(True, axis='y')
-    plt.xticks(x, monthly_balance.index.astype(str), rotation=45)
+    plt.xticks(x, periodic_balance.index.astype(str), rotation=45)
     plt.tight_layout()
     st.pyplot(plt)
 
@@ -142,14 +143,15 @@ show_adjusted = False
 #show_adjusted = st.sidebar.checkbox("Show Adjusted Data", value=False)
 show_income = st.sidebar.checkbox("Show Income", value=True)
 show_expenses = st.sidebar.checkbox("Show Expenses", value=True)
-show_data_tables = st.sidebar.checkbox("Show Data Tables", value=True)
+show_data_tables = st.sidebar.checkbox("Show Data Tables", value=False)
 
 # If both files are uploaded
 if uploaded_files:
     combined_data = load_data(uploaded_files)
     # Sidebar for user inputs
     st.sidebar.header('Filter Options')
-
+    period_name = st.sidebar.selectbox("Period", list(periods.values()), index=2)
+    period = periods.inverse[period_name]
     # Date range selection
     start_date = st.sidebar.date_input('Start Date', combined_data['Date'].min())
     end_date = st.sidebar.date_input('End Date', combined_data['Date'].max())
@@ -165,33 +167,33 @@ if uploaded_files:
         st.sidebar.error('Error: Start date must be before end date.')
 
     if filtered_df is not None:
-        filtered_monthly_balance, adjusted_monthly_balance = filter_and_adjust_data(filtered_df)
+        filtered_periodic_balance, adjusted_periodic_balance = filter_and_adjust_data(filtered_df, period=period)
 
         # Calculate monthly income and expenses
-        monthly_income_expenses = calculate_monthly_income_expenses(filtered_df)
+        periodic_income_expenses = calculate_periodic_income_expenses(filtered_df, period=period)
         
         # Calculate adjustments
         removed_rows = filtered_df[filtered_df['Description'].str.contains("GBP to", na=False)]
-        monthly_adjustments = -removed_rows.groupby(removed_rows['Date'].dt.to_period('M'))['Amount'].sum()
+        periodic_adjustments = -removed_rows.groupby(removed_rows['Date'].dt.to_period(period))['Amount'].sum()
         
         # Calculate adjusted income and expenses
-        adjusted_income_expenses = adjust_income_expenses(monthly_income_expenses, monthly_adjustments)
+        adjusted_income_expenses = adjust_income_expenses(periodic_income_expenses, periodic_adjustments)
 
         # Calculate net changes
-        filtered_net_change = calculate_net_changes(filtered_monthly_balance)
-        adjusted_net_change = calculate_net_changes(adjusted_monthly_balance)
+        filtered_net_change = calculate_net_changes(filtered_periodic_balance)
+        adjusted_net_change = calculate_net_changes(adjusted_periodic_balance)
 
         # Calculate cumulative net changes
         filtered_cumulative_net_changes = calculate_cumulative_net_changes(filtered_net_change)
         adjusted_cumulative_net_changes = calculate_cumulative_net_changes(adjusted_net_change)
 
         # Calculate percentage changes
-        filtered_percentage_change = calculate_percentage_changes(filtered_monthly_balance)
-        adjusted_percentage_change = calculate_percentage_changes(adjusted_monthly_balance)
+        filtered_percentage_change = calculate_percentage_changes(filtered_periodic_balance)
+        adjusted_percentage_change = calculate_percentage_changes(adjusted_periodic_balance)
 
-        # Plot end-of-month balances
-        st.subheader("End-of-Month Balance Evolution")
-        plot_data(filtered_monthly_balance, adjusted_monthly_balance, "End-of-Month Balance", "Balance (GBP)", show_income=show_income, show_adjusted=show_adjusted)
+        # Plot end-of-period balances
+        st.subheader(f"End-of-{period_name} Balance Evolution")
+        plot_data(filtered_periodic_balance, adjusted_periodic_balance, f"End-of-{period_name} Balance", "Balance (GBP)", show_income=show_income, show_adjusted=show_adjusted)
 
         # Plot net changes
         st.subheader("Net Change")
@@ -206,8 +208,8 @@ if uploaded_files:
         plot_data(filtered_percentage_change, adjusted_percentage_change, "Month-over-Month Percentage Change", "Percentage Change (%)", income_lable='Percentage Change', adjusted_lable='Adjusted Percentage Change', show_income=show_income, show_adjusted=show_adjusted)
 
         # Plot histogram of income and expenses
-        st.subheader("Monthly Income and Expenses")
-        plot_histogram(monthly_income_expenses, adjusted_income_expenses, 
+        st.subheader("Periodic Income and Expenses")
+        plot_histogram(periodic_income_expenses, adjusted_income_expenses, period=period,
                        adjusted=show_adjusted, expenses=show_expenses, income=show_income)
 
         # Display averages
@@ -215,15 +217,15 @@ if uploaded_files:
         st.write(f"Average Net Change: {filtered_net_change.mean():.2f} GBP")
         if show_adjusted:
             st.write(f"Average Adjusted Net Change: {adjusted_net_change.mean():.2f} GBP")
-        st.write(f"Average Income: {monthly_income_expenses['Income'].mean():.2f} GBP")
+        st.write(f"Average Income: {periodic_income_expenses['Income'].mean():.2f} GBP")
         if show_adjusted:
             st.write(f"Average Adjusted Income: {adjusted_income_expenses['Income'].mean():.2f} GBP")
-        st.write(f"Average Expenses: {monthly_income_expenses['Expenses'].mean():.2f} GBP")
+        st.write(f"Average Expenses: {periodic_income_expenses['Expenses'].mean():.2f} GBP")
         if show_adjusted:
             st.write(f"Average Adjusted Expenses: {adjusted_income_expenses['Expenses'].mean():.2f} GBP")
 
         st.subheader("Cumulative Percentage Change")
-        cumulative_pct_change = calculate_cumulative_percentage_change(monthly_income_expenses['Income'])
+        cumulative_pct_change = calculate_cumulative_percentage_change(periodic_income_expenses['Income'])
         st.write(f"Cumulative Percentage Change in Income: {cumulative_pct_change:.2f}%")
         if show_adjusted:
             adjusted_cumulative_pct_change = calculate_cumulative_percentage_change(adjusted_income_expenses['Income'])
@@ -237,21 +239,21 @@ if uploaded_files:
 
             # Display monthly adjustments
             if show_adjusted:
-                st.subheader("Monthly Adjustments")
-                st.write(monthly_adjustments)
+                st.subheader("Periodic Adjustments")
+                st.write(periodic_adjustments)
 
             # Display filtered monthly balance
-            st.subheader("Filtered Monthly Balance")
-            st.write(filtered_monthly_balance)
+            st.subheader("Filtered Periodic Balance")
+            st.write(filtered_periodic_balance)
 
             # Display adjusted monthly balance
             if show_adjusted:
-                st.subheader("Adjusted Monthly Balance")
-                st.write(adjusted_monthly_balance)
+                st.subheader("Adjusted Periodic Balance")
+                st.write(adjusted_periodic_balance)
 
             # Display monthly income and expenses
-            st.subheader("Monthly Income and Expenses")
-            st.write(monthly_income_expenses)
+            st.subheader("Periodic Income and Expenses")
+            st.write(periodic_income_expenses)
 
             # Display adjusted income and expenses
             if show_adjusted:
@@ -285,18 +287,18 @@ if uploaded_files:
                 st.subheader("Adjusted Percentage Changes")
                 st.write(adjusted_percentage_change)
 
-            # Display filtered monthly balance
-            st.subheader("Filtered Monthly Balance")
-            st.write(filtered_monthly_balance)
+            # Display filtered periodic balance
+            st.subheader("Filtered Periodic Balance")
+            st.write(filtered_periodic_balance)
 
             # Display adjusted monthly balance
             if show_adjusted:
-                st.subheader("Adjusted Monthly Balance")
-                st.write(adjusted_monthly_balance)
+                st.subheader("Adjusted Periodic Balance")
+                st.write(adjusted_periodic_balance)
 
-            # Display monthly income and expenses
-            st.subheader("Monthly Income and Expenses")
-            st.write(monthly_income_expenses)
+            # Display periodic income and expenses
+            st.subheader("Periodic Income and Expenses")
+            st.write(periodic_income_expenses)
 
             # Display adjusted income and expenses
             if show_adjusted:
